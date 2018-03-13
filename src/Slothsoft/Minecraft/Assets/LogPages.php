81a4119be9e1eabd9fa24a3b4ab0f7735dc6d6a2
Faulo <FauloLio@gmail.com>
2018-03-13 15:57:30 +0100

    declare(strict_types = 1);

--- src/Slothsoft/Minecraft/Assets/LogPages.php
+++ src/Slothsoft/Minecraft/Assets/LogPages.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 namespace Slothsoft\Minecraft\Assets;
 
 use Slothsoft\Farah\Module\FarahUrl\FarahUrl;
--- src/Slothsoft/Minecraft/NBT/TAGEnd.php
+++ src/Slothsoft/Minecraft/NBT/TAGEnd.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 namespace Slothsoft\Minecraft\NBT;
 
 class TAGEnd extends TAGNode
